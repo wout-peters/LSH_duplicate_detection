9e94b9fc29613a6e915a98794c07d00d26c1e9cf
woutp <wout.peters6@gmail.com>
2023-11-22 18:06:15 +0100

    Create clean df, with features enclosed in list

---
+++ LSH.py
@@ -0,0 +1,35 @@
+# author: Wout Peters (531818)
+
+import json
+import numpy as np
+import pandas as pd
+import random
+
+
+def create_df(data):
+    raw_df = pd.json_normalize(data).transpose()
+    df = pd.DataFrame(columns = ['shop', 'url', 'modelID', 'title', 'features'])
+    for i in range(len(raw_df)):
+        for j in range(len(raw_df[0][i])):
+            row = [raw_df[0][i][j]['shop'], 
+                    raw_df[0][i][j]['url'], 
+                    raw_df[0][i][j]['modelID'], 
+                    raw_df[0][i][j]['title'], 
+                    raw_df[0][i][j]['featuresMap']]
+            df.loc[len(df.index)] = row
+    return df
+
+
+def main():
+    with open("D:/Studie/23-24/Blok 2/Computer Science/Personal Assignment/TVs-all-merged (1)/TVs-all-merged.json", 'r') as read_file:
+        data = json.load(read_file)
+    read_file.close()
+
+    df = create_df(data)
+
+    print(df.head())
+
+    
+
+if __name__ == "__main__":
+    main()
